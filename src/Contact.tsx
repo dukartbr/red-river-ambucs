8da22a54f1777692c24f4612b9b8e9a63d617eee
Brady Dukart <dukartbrady@gmail.com>
2024-04-24 18:05:46 -0500

    type fix

--- src/Contact.tsx
+++ src/Contact.tsx
@@ -8,7 +8,7 @@ import {
   ListIcon,
   UnorderedList,
 } from "@chakra-ui/react";
-import { FaPhoneFlip, FaRegEnvelope, FaRegNewspaper } from "react-icons/fa6";
+import { FaPhoneFlip, FaRegEnvelope } from "react-icons/fa6";
 
 const resources = [
   {
